08078170b2363479bb54e8f621a639a669ca50f2
Rony Reyes <rony@caidci.com>
2026-01-01 15:49:53 -0800

    adding the site navigation

---
+++ scripts/navigation.js
@@ -0,0 +1,7 @@
+//store the selected elements that we are going to use 
+const navbuttom = document.querySelector('#ham-btn');
+
+//Toggle the show class of and on 
+navbuttom.addEventListener('click', () => {
+    navbuttom.classList.toggle('show');
+});
