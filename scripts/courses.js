d02bf1fdbc1237dad98af38b3fa957e65b371fe4
Rony Reyes <rony@caidci.com>
2026-01-02 15:03:02 -0800

    adding the section of courses to show just the subject and number from the js file

--- scripts/courses.js
+++ scripts/courses.js
@@ -1,8 +1,8 @@
 const courses = [
-    { subject: "CSE", number: 110, title: "Intro to Programming", credits: 2, completed: false },
+    { subject: "CSE", number: 110, title: "Intro to Programming", credits: 2, completed: true },
     { subject: "WDD", number: 130, title: "Web Fundamentals", credits: 2, completed: true },
-    { subject: "CSE", number: 111, title: "Programming with Functions", credits: 2, completed: false },
-    { subject: "CSE", number: 210, title: "Programming with Classes", credits: 2, completed: false },
+    { subject: "CSE", number: 111, title: "Programming with Functions", credits: 2, completed: true },
+    { subject: "CSE", number: 210, title: "Programming with Classes", credits: 2, completed: true },
     { subject: "WDD", number: 131, title: "Dynamic Web Fundamentals", credits: 2, completed: true },
     { subject: "WDD", number: 231, title: "Frontend Web Development I", credits: 2, completed: false }
 ];
@@ -26,8 +26,6 @@ function displayCourses(list) {
 
         div.innerHTML = `
             <h3>${course.subject} ${course.number}</h3>
-            <p>${course.title}</p>
-            <p>Credits: ${course.credits}</p>
         `;
 
         courseContainer.appendChild(div);
