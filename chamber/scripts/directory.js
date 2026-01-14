cd1d85c22619c036e3a335584d51c994f515d218
Rony Reyes <rony@caidci.com>
2026-01-14 15:17:59 -0800

    adding the hamguer style for the javascript and adding the class combing the titles and nav

--- chamber/scripts/directory.js
+++ chamber/scripts/directory.js
@@ -4,6 +4,16 @@ document.getElementById("currentyear").textContent =
 document.getElementById("lastModified").textContent =
     `Last Modified: ${document.lastModified}`;
 
+const navbuttom = document.querySelector('#ham-btn');
+const navlinks = document.querySelector('#nav-bar');
+
+navbuttom.addEventListener('click', () => {
+    navbuttom.classList.toggle('show');
+    navlinks.classList.toggle('show');
+});
+
+
+
 const membersContainer = document.querySelector("#members-container");
 
 async function GetMembers() {
