c35ca3f59d8d5e949dd938b8d94ff14625915dca
Rony Reyes <rony@caidci.com>
2026-01-14 14:55:38 -0800

    trying the getmember data

--- chamber/scripts/directory.js
+++ chamber/scripts/directory.js
@@ -3,3 +3,13 @@ document.getElementById("currentyear").textContent =
 
 document.getElementById("lastModified").textContent =
     `Last Modified: ${document.lastModified}`;
+
+const membersContainer = document.querySelector("#members-container");
+
+async function GetMembers() {
+    const response = await fetch("data/members.json");
+    const data = await response.json();
+    console.log(data);
+}
+
+GetMembers();
