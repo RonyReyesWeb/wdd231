1d9f0417b14bd155f4b4db5ea1b7b9985bc4bc9f
Rony Reyes <rony@caidci.com>
2026-01-16 15:53:05 -0800

    adding the logic of the code

--- scripts/weatherapp.js
+++ scripts/weatherapp.js
@@ -1,24 +1,20 @@
 const Mytown = document.querySelector('#town');
-const myGraphic = document.querySelector('#Graphic');
+const myGraphic = document.querySelector('#graphic');
 const myDescription = document.querySelector('#description');
 const myTemperature = document.querySelector('#temperature');
-
 
 const myKey = "da842027fc309ca7e4df058c9bf101fc"
 const myLat = "14.631"
 const myLong = "-90.516"
-
 const MyURL = `//api.openweathermap.org/data/2.5/weather?lat=${myLat}&lon=${myLong}&appid=${myKey}&units=imperial`
 
 
-// `
 async function apiFetch() {
     try {
         const response = await fetch(MyURL);
         if (response.ok) {
             const data = await response.json();
-            console.log(data); // testing only
-            // displayResults(data); // uncomment when ready
+            displayResults(data);
         } else {
             throw Error(await response.text());
         }
@@ -28,7 +24,14 @@ async function apiFetch() {
 }
 
 function displayResults(data) {
-    console.log('hello')
+    Mytown.innerHTML = data.name
+    myDescription.innerHTML = data.weather[0].description
+    myTemperature.innerHTML = `${data.main.temp}&deg;F`
+    const iconsrc = `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`
+    myGraphic.setAttribute('SRC', iconsrc)
+    myGraphic.setAttribute('alt', data.weather[0].description)
+
+
 }
 
 
