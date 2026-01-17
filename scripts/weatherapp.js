69b0c2f4f458132e434004e5cbe128b04fd1500b
Rony Reyes <rony@caidci.com>
2026-01-16 16:36:30 -0800

    deleting the key just for educational purpose

--- scripts/weatherapp.js
+++ scripts/weatherapp.js
@@ -3,7 +3,7 @@ const myGraphic = document.querySelector('#graphic');
 const myDescription = document.querySelector('#description');
 const myTemperature = document.querySelector('#temperature');
 
-const myKey = "da842027fc309ca7e4df058c9bf101fc"
+const myKey = "YOUR_API_KEY_HERE";
 const myLat = "14.631"
 const myLong = "-90.516"
 const MyURL = `//api.openweathermap.org/data/2.5/weather?lat=${myLat}&lon=${myLong}&appid=${myKey}&units=imperial`
