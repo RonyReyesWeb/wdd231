2d66324c5c490ed0129b68d43b69bb21186b550d
Rony Reyes <rony@caidci.com>
2026-01-16 21:35:31 -0800

    adding the weather part to my code

--- chamber/scripts/weather.js
+++ chamber/scripts/weather.js
@@ -2,8 +2,10 @@ const Mytown = document.querySelector('#town');
 const myGraphic = document.querySelector('#graphic');
 const myDescription = document.querySelector('#description');
 const myTemperature = document.querySelector('#temperature');
+const myHumidity = document.querySelector('#humidity');
 
-const myKey = "";
+
+const myKey = "da842027fc309ca7e4df058c9bf101fc";
 const myLat = "14.631"
 const myLong = "-90.516"
 const MyURL = `//api.openweathermap.org/data/2.5/weather?lat=${myLat}&lon=${myLong}&appid=${myKey}&units=imperial`
@@ -14,6 +16,7 @@ async function apiFetch() {
         const response = await fetch(MyURL);
         if (response.ok) {
             const data = await response.json();
+            console.log(data);
             displayResults(data);
         } else {
             throw Error(await response.text());
@@ -24,13 +27,33 @@ async function apiFetch() {
 }
 
 function displayResults(data) {
+    console.log('hello')
     Mytown.innerHTML = data.name
-    myDescription.innerHTML = data.weather[0].description
-    myTemperature.innerHTML = `${data.main.temp}&deg;F`
+
+    // data to handle the img 
     const iconsrc = `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`
-    myGraphic.setAttribute('SRC', iconsrc)
+    myGraphic.setAttribute('src', iconsrc)
     myGraphic.setAttribute('alt', data.weather[0].description)
 
+    // data to handle the condition/description 
+    myDescription.innerHTML = data.weather[0].description
+
+    // to handle the temperature
+    const tempF = data.main.temp;
+    const tempC = Math.round((tempF - 32) * (5 / 9));
+    myTemperature.innerHTML = `~${tempC}&deg;C (${Math.round(tempF)}&deg;F)`;
+
+    // data to handle the mudidity 
+    const humidity = data.main.humidity;
+    let humidityText;
+    if (humidity < 30) {
+        humidityText = "Low";
+    } else if (humidity < 70) {
+        humidityText = "Moderate";
+    } else {
+        humidityText = "High";
+    }
+    myHumidity.innerHTML = `${humidityText} (${humidity}%)`;
 
 }
 
