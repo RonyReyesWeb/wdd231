adf545e3581cb904271bdb9ec5e28e347b407397
Rony Reyes <rony@caidci.com>
2026-01-21 12:07:44 -0800

    fixing the img error for not having information on it, implementing the wayfinder functionality

--- chamber/scripts/directory.js
+++ chamber/scripts/directory.js
@@ -74,5 +74,22 @@ listBtn.addEventListener("click", () => {
     membersContainer.classList.add("list-view"); // list mode
 });
 
+
+
+// Adding the wayfinder logic
+document.addEventListener("DOMContentLoaded", () => {
+    let currentPage = window.location.pathname.split("/").pop();
+    if (currentPage === "") {
+        currentPage = "index.html";
+    }
+
+    document.querySelectorAll(".navigation a").forEach(link => {
+        if (link.getAttribute("href") === currentPage) {
+            link.classList.add("active");
+            link.setAttribute("aria-current", "page");
+        }
+    });
+});
+
 // Run on page load
 getMembers();
