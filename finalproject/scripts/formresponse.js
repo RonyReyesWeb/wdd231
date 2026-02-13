38ef55a547d5d601b321aee54e1f228d808e832e
Rony Reyes <rony@caidci.com>
2026-02-12 19:57:01 -0800

    adding the contact form

--- finalproject/scripts/formresponse.js
+++ finalproject/scripts/formresponse.js
@@ -2,8 +2,10 @@ const params = new URLSearchParams(window.location.search);
 
 const fullname = params.get('fullname');
 const email = params.get('email');
+const service = params.get('service');
 const message = params.get('message');
 
 document.getElementById('display-name').textContent = fullname;
 document.getElementById('display-email').textContent = email;
+document.getElementById('display-service').textContent = service;
 document.getElementById('display-message').textContent = message;
