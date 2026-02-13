db8345c05e8bf7f683d2937395e79e2349cdffb0
Rony Reyes <rony@caidci.com>
2026-02-12 16:17:28 -0800

    adding the design for the div in contact

--- finalproject/scripts/formresponse.js
+++ finalproject/scripts/formresponse.js
@@ -1,9 +1,9 @@
 const params = new URLSearchParams(window.location.search);
 
-const name = params.get('name');
+const fullname = params.get('fullname');
 const email = params.get('email');
 const message = params.get('message');
 
-document.getElementById('display-name').textContent = name;
+document.getElementById('display-name').textContent = fullname;
 document.getElementById('display-email').textContent = email;
 document.getElementById('display-message').textContent = message;
